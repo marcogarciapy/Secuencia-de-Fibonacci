3104dd70aff3dde63c8535e0a7a683357d6e4a00
marcogarciapy <marcogarcia2004.10@gmail.com>
2025-09-05 19:57:29 -0700

    Add files via upload

---
+++ secuenciaFibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    a, b = 0, 1
+    sequence = []
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+num_terms = int(input("Ingrese el número de términos de la secuencia Fibonacci que desea: "))
+fib_sequence = fibonacci(num_terms)
+print(f"Secuencia Fibonacci con {num_terms} términos: {fib_sequence}")
